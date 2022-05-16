d27d0c0a0c7897b224d146e9b04ce71b1b559598
fomka94 <fom-aina@mail.ru>
2022-05-17 00:57:57 +0900

    Initial Commit

---
+++ timerPomodoro1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timerPomodoro1
+//
+//  Created by Фомина Айина Валерьевна on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ timerPomodoro1UITests/timerPomodoro1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  timerPomodoro1UITestsLaunchTests.swift
+//  timerPomodoro1UITests
+//
+//  Created by Фомина Айина Валерьевна on 17.05.2022.
+//
+
+import XCTest
+
+class timerPomodoro1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
